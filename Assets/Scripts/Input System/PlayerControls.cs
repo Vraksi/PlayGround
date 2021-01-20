c614ee88d56ed5144ba3d8a93316d02a849d0962
Vraksi <Jakobmetalv@gmail.com>
2021-01-20 20:42:01 +0100

    Player controls implementation

---
+++ Assets/Scripts/Input System/PlayerControls.cs
@@ -0,0 +1,182 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""872c5c00-6a45-44ae-a8ab-f241b19c0120"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""903ed790-6862-49a7-8113-78bf3b9a0b63"",
+                    ""expectedControlType"": ""Stick"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""DebugLog"",
+                    ""type"": ""Button"",
+                    ""id"": ""25ef7d45-1dcd-4b31-b886-a01bb822d1f3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""5ac35718-e5b5-493a-aec5-6446f66e2335"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""New control scheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ae0c75ec-1e2b-42e1-adca-4b7d7965a788"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""New control scheme"",
+                    ""action"": ""DebugLog"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""New control scheme"",
+            ""bindingGroup"": ""New control scheme"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // GamePlay
+        m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+        m_GamePlay_Movement = m_GamePlay.FindAction("Movement", throwIfNotFound: true);
+        m_GamePlay_DebugLog = m_GamePlay.FindAction("DebugLog", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // GamePlay
+    private readonly InputActionMap m_GamePlay;
+    private IGamePlayActions m_GamePlayActionsCallbackInterface;
+    private readonly InputAction m_GamePlay_Movement;
+    private readonly InputAction m_GamePlay_DebugLog;
+    public struct GamePlayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GamePlayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_GamePlay_Movement;
+        public InputAction @DebugLog => m_Wrapper.m_GamePlay_DebugLog;
+        public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+        public void SetCallbacks(IGamePlayActions instance)
+        {
+            if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @DebugLog.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnDebugLog;
+                @DebugLog.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnDebugLog;
+                @DebugLog.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnDebugLog;
+            }
+            m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @DebugLog.started += instance.OnDebugLog;
+                @DebugLog.performed += instance.OnDebugLog;
+                @DebugLog.canceled += instance.OnDebugLog;
+            }
+        }
+    }
+    public GamePlayActions @GamePlay => new GamePlayActions(this);
+    private int m_NewcontrolschemeSchemeIndex = -1;
+    public InputControlScheme NewcontrolschemeScheme
+    {
+        get
+        {
+            if (m_NewcontrolschemeSchemeIndex == -1) m_NewcontrolschemeSchemeIndex = asset.FindControlSchemeIndex("New control scheme");
+            return asset.controlSchemes[m_NewcontrolschemeSchemeIndex];
+        }
+    }
+    public interface IGamePlayActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnDebugLog(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    private PlayerControls controls;
+    private Vector2 move;
+    
     public float maxSpeed = 7;
 
     Rigidbody2D rb2d;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        controls = new PlayerControls();
         rb2d = GetComponent<Rigidbody2D>();
+        
+        controls.GamePlay.Movement.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.GamePlay.Movement.canceled += ctx => move = Vector2.zero;
 
     }
 
@@ -23,18 +31,19 @@ public class Movement : MonoBehaviour
 
     private void HorizontalMovement()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
-        }
+        Vector2 currentMoveSpeed = new Vector2(move.x * maxSpeed, move.y * maxSpeed);
+        rb2d.velocity = currentMoveSpeed;
+    }
 
-        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-        {
-            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
-        }
+    //Skal være med for Enable vores movement input control
+    private void OnEnable()
+    {
+        controls.GamePlay.Enable();
+    }
+
+    //Skal være med for Disable vores movement input control
+    private void OnDisable()
+    {
+        controls.GamePlay.Disable();
     }
 }
