9d0c34de2166267f39330664dbf6c86b917d582e
Vraksi <Jakobmetalv@gmail.com>
2021-01-26 19:47:28 +0100

    BlockBreaker 77.

--- Assets/BlockBreaker/Scripts/Block.cs
+++ Assets/BlockBreaker/Scripts/Block.cs
@@ -1,30 +1,84 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    //Config params (Variabler vi ændrer på manuelt)
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
 
-    //cached Reference
+    //cached Reference (Variabler for ting vi gerne vil have fat i vores script)
     Level level;
+    GameStatus gameStatus;
+
+    //state variables (Variabler der angiver den status objektet har)
+    [SerializeField] int timesHit; // only serialized for debugging.
 
     private void Start()
     {
-        level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        CountBreakableBlocks();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //playclipatpoint laver en audio source i 3d rum og skal sættes på cameraet fordi ellers bliver lyden lavere.
-        DestroyBlock();
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
     }
-    
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
     private void DestroyBlock()
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        PlayBlockDestroySFX();
         Destroy(gameObject);
         level.BlockDestroyed();
+        TriggerSparklesVFX();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+
+        if (tag == "Breakable")
+        {
+            level.CountBreakableBlocks();
+        }
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
+        //playclipatpoint laver en audio source i 3d rum og skal sættes på cameraet fordi ellers bliver lyden lavere.
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        //Vi er nød til lægge den i en variable for kunne slette den.
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 2f);
     }
 }
---
+++ Assets/BlockBreaker/Scripts/GameStatus.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+    //config params
+    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointPerBlockDestroyed = 10;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    //state Variables
+    [SerializeField] int score = 0;
+
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            //skal muligvis bruges hvis Singleton tror der er flere end 2 af dem.
+            //gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.SetText(score.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        score += pointPerBlockDestroyed;
+        scoreText.SetText(score.ToString());
+    }
+
+    public void GameOver()
+    {
+        Destroy(gameObject);
+    }
+}
